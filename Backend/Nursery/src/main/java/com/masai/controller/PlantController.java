67170ec941d9db840ae57b5feb09e0e8862d2c96
Deshmukh-Tushar <89882720+deshmukh-tushar@users.noreply.github.com>
2023-11-18 13:44:58 +0530

    Update PlantController.java
    added comments to methods

--- Backend/Nursery/src/main/java/com/masai/controller/PlantController.java
+++ Backend/Nursery/src/main/java/com/masai/controller/PlantController.java
@@ -25,68 +25,70 @@ import jakarta.validation.Valid;
 @CrossOrigin(origins = "*")
 public class PlantController {
 
-	@Autowired
-	private PlantServiceImpl plantService;
-	
-	@PostMapping("/plants")
-	public ResponseEntity<Plant> addNewPlant(@Valid @RequestBody Plant plant) throws PlantException{
-		
-		Plant newPlant = plantService.addPlant(plant);
-		
-		return new ResponseEntity<Plant>(newPlant,HttpStatus.CREATED);
-	}
-	
-	@PutMapping("/plants/{plantId}")
-	public ResponseEntity<Plant> updatePlant(@Valid @PathVariable Integer plantId, @RequestBody Plant plant) throws PlantException{ 
-		
-		Plant existingPlant = plantService.updatePlant(plantId, plant);
-		
-		return new ResponseEntity<Plant>(existingPlant,HttpStatus.OK);
-	}
-	
-	@DeleteMapping("/plants/{id}")
-	public ResponseEntity<Plant> deletePlant(@PathVariable("id") Integer plantId) throws PlantException{
-		
-		Plant existingPlant = plantService.deletePlant(plantId);
-		
-		return new ResponseEntity<Plant>(existingPlant,HttpStatus.OK);
-		
-	}
-	
-	@GetMapping("/plants/{plantId}")
-	public ResponseEntity<Plant> getPlantById(@PathVariable Integer plantId) throws PlantException{
-		
-		Plant existingPlant = plantService.viewPlant(plantId);
-		
-		return new ResponseEntity<Plant>(existingPlant,HttpStatus.FOUND);
-		
-	}
-	
-	@GetMapping("/plants")
-	public ResponseEntity<List<Plant>> findAllPlants() throws PlantException{
-		
-		List<Plant> plantList = plantService.viewAllPlants();
-		
-		return new ResponseEntity<List<Plant>>(plantList,HttpStatus.FOUND);
-	}
-	
-	@GetMapping("/plantByCommonName/{commonName}")
-	public ResponseEntity<List<Plant>> findSeedByCommonName(@PathVariable String commonName) throws PlantException{
-		
-		List<Plant> plantList = plantService.viewPlant(commonName);
-		
-		return new ResponseEntity<List<Plant>>(plantList,HttpStatus.FOUND);		
-	}
-	
-	@GetMapping("/plantsByTypeOfPlant/{typeOfPlant}")
-	public ResponseEntity<List<Plant>> findSeedByTypeOfPlant(@PathVariable String typeOfPlant) throws PlantException{
-		
-		List<Plant> plantList = plantService.viewAllPlants(typeOfPlant);
-		
-		return new ResponseEntity<List<Plant>>(plantList,HttpStatus.FOUND);		
-	}
-	
-	
-	
-	
+    @Autowired
+    private PlantServiceImpl plantService;
+
+    // Method to add a new plant
+    @PostMapping("/plants")
+    public ResponseEntity<Plant> addNewPlant(@Valid @RequestBody Plant plant) throws PlantException {
+        // Call the service to add a new plant
+        Plant newPlant = plantService.addPlant(plant);
+        // Return the new plant with HTTP status 201 (Created)
+        return new ResponseEntity<Plant>(newPlant, HttpStatus.CREATED);
+    }
+
+    // Method to update an existing plant by ID
+    @PutMapping("/plants/{plantId}")
+    public ResponseEntity<Plant> updatePlant(@Valid @PathVariable Integer plantId, @RequestBody Plant plant)
+            throws PlantException {
+        // Call the service to update an existing plant
+        Plant existingPlant = plantService.updatePlant(plantId, plant);
+        // Return the updated plant with HTTP status 200 (OK)
+        return new ResponseEntity<Plant>(existingPlant, HttpStatus.OK);
+    }
+
+    // Method to delete a plant by ID
+    @DeleteMapping("/plants/{id}")
+    public ResponseEntity<Plant> deletePlant(@PathVariable("id") Integer plantId) throws PlantException {
+        // Call the service to delete a plant
+        Plant existingPlant = plantService.deletePlant(plantId);
+        // Return the deleted plant with HTTP status 200 (OK)
+        return new ResponseEntity<Plant>(existingPlant, HttpStatus.OK);
+    }
+
+    // Method to get a plant by ID
+    @GetMapping("/plants/{plantId}")
+    public ResponseEntity<Plant> getPlantById(@PathVariable Integer plantId) throws PlantException {
+        // Call the service to view a specific plant by ID
+        Plant existingPlant = plantService.viewPlant(plantId);
+        // Return the plant with HTTP status 302 (Found)
+        return new ResponseEntity<Plant>(existingPlant, HttpStatus.FOUND);
+    }
+
+    // Method to find all plants
+    @GetMapping("/plants")
+    public ResponseEntity<List<Plant>> findAllPlants() throws PlantException {
+        // Call the service to view all plants
+        List<Plant> plantList = plantService.viewAllPlants();
+        // Return the list of plants with HTTP status 302 (Found)
+        return new ResponseEntity<List<Plant>>(plantList, HttpStatus.FOUND);
+    }
+
+    // Method to find plants by common name
+    @GetMapping("/plantByCommonName/{commonName}")
+    public ResponseEntity<List<Plant>> findSeedByCommonName(@PathVariable String commonName) throws PlantException {
+        // Call the service to view plants by common name
+        List<Plant> plantList = plantService.viewPlant(commonName);
+        // Return the list of plants with HTTP status 302 (Found)
+        return new ResponseEntity<List<Plant>>(plantList, HttpStatus.FOUND);
+    }
+
+    // Method to find plants by type of plant
+    @GetMapping("/plantsByTypeOfPlant/{typeOfPlant}")
+    public ResponseEntity<List<Plant>> findSeedByTypeOfPlant(@PathVariable String typeOfPlant) throws PlantException {
+        // Call the service to view plants by type of plant
+        List<Plant> plantList = plantService.viewAllPlants(typeOfPlant);
+        // Return the list of plants with HTTP status 302 (Found)
+        return new ResponseEntity<List<Plant>>(plantList, HttpStatus.FOUND);
+    }
 }
